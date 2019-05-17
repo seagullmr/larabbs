d6084f4199c346a9095ce0d84f2e24267438f306
seagull <seagullmr@163.com>
2019-05-17 09:52:29 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 // 个人页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
