06bdb7878924dbf933433ae8a268d92c10140d43
seagull <seagullmr@163.com>
2019-06-13 14:08:47 +0800

    活跃用户

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -5,8 +5,20 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Dingo\Api\Routing\Helpers;
 use App\Http\Controllers\Controller as BaseController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Controller extends BaseController
 {
     use Helpers;
+
+    /**
+     * 接口错误信息提示
+     * @param $statusCode 服务器响应状态
+     * @param null $message 响应提示
+     * @param int $code 错误码
+     */
+    public function errorResponse($statusCode, $message=null, $code=0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ $api->version('v2', function($api) {
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => ['serializer:array', 'bindings']
+    'middleware' => ['serializer:array', 'bindings', 'change-locale']
 ], function($api) {
 
     $api->group([
@@ -89,6 +89,12 @@ $api->version('v1', [
             ->name('api.topics.replies.index'); // 话题回复列表
         $api->get('users/{user}/replies', 'RepliesController@userIndex')
             ->name('api.users.replies.index'); // 某个用户的回复列表
+        // 资源推荐
+        $api->get('links', 'LinksController@index')
+            ->name('api.links.index');
+        // 活跃用户
+        $api->get('actived/users', 'UsersController@activedIndex')
+            ->name('api.actived.users.index');
 
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
